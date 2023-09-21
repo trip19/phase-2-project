f69ee50c0b0cdd47f00d500c0039eeec7d8d7448
trip19 <66626402+trip19@users.noreply.github.com>
2023-09-20 23:06:45 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import PokemonList from './PokemonList';
+import Pagination from './Pagination';
+import SearchBar from './SearchBar';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [totalPages, setTotalPages] = useState(1);
+
+  const pokemonsPerPage = 10;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1000');
+      const data = await response.json();
+      setPokemonData(data.results);
+      setTotalPages(Math.ceil(data.results.length / pokemonsPerPage));
+    };
+    fetchData();
+  }, []);
+
+  const handleSearch = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    setCurrentPage(1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon Search App</h1>
+      <SearchBar onSearch={handleSearch} />
+      <PokemonList
+        pokemonData={pokemonData}
+        currentPage={currentPage}
+        pokemonsPerPage={pokemonsPerPage}
+        searchTerm={searchTerm}
+      />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={(newPage) => setCurrentPage(newPage)}
+      />
     </div>
   );
 }
